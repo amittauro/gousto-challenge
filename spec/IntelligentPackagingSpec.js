61cb2f29e3d0f88f608e13cee432bbae9fa19a49
Amit Tauro <tauroamit@gmail.com>
2020-11-19 11:23:08 +0000

    gousto set up

---
+++ spec/IntelligentPackagingSpec.js
@@ -0,0 +1,9 @@
+describe("IntelligentPackaging", () => {
+  let intelligentPackaging
+
+  beforeEach(() => {
+    intelligentPackaging = new IntelligentPackaging(mockorders, mockboxes)
+  })
+
+  it('')
+})
